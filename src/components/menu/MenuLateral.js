206fb1c9a5accfdff1932f4ddb60be6134bcb798
André <andreacciolyl@hotmail.com>
2022-09-09 21:50:53 -0300

    Sidebar menu

--- src/components/menu/MenuLateral.js
+++ src/components/menu/MenuLateral.js
@@ -1,12 +1,45 @@
-import * as React from 'react';
+    import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom";
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 function LinkMenu(props) {
     const classes = props.classes;
     return( 
         <div className={classes.menuLateral}>
+            <div className={classes.menuLateralTopo}></div>
+            <div className={classes.menuLateralItens}>
 
+                <div className={classes.menuLateralItemArrow}>
+                    <ArrowRightIcon className={classes.menuLateralIconArrow}/>
+                    <div className={classes.menuLateralItem}>Meu perfil</div>
+                </div>
+                <div className={classes.menuLateralItemArrow}>
+                    <ArrowRightIcon className={classes.menuLateralIconArrow}/>
+                    <div className={classes.menuLateralItem}>Exames e consultas</div>
+                </div>
+                <div className={classes.menuLateralItemArrow}>
+                    <ArrowRightIcon className={classes.menuLateralIconArrow}/>
+                    <div className={classes.menuLateralItem}>Financeiro</div>
+                </div>
+
+                <div className={classes.menuLinha}></div>
+
+                <div className={classes.menuLateralItemArrow}>
+                    <ArrowRightIcon className={classes.menuLateralIconArrow}/>
+                    <div className={classes.menuLateralItem}>Cadastrar cliente</div>
+                </div>
+                <div className={classes.menuLateralItemArrow}>
+                    <ArrowRightIcon className={classes.menuLateralIconArrow}/>
+                    <div className={classes.menuLateralItem}>Consultório</div>
+                </div>
+                <div className={classes.menuLateralItemArrow}>
+                    <ArrowRightIcon className={classes.menuLateralIconArrow}/>
+                    <div className={classes.menuLateralItem}>Agenda</div>
+                </div>
+                
+            </div>
+            <div className={classes.versao}>Version 0.0.0</div>
         </div>
     )
 }
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,9 @@
 import { makeStyles } from "@material-ui/styles"
 
+const corAzul1 = '#cfedfc'
+const corAzul2 = '#98d4f5'
+const corAzul3 = '#4791ba'
+
 const useStyles = makeStyles({
     root: {
         display: 'flex',
@@ -11,7 +15,7 @@ const useStyles = makeStyles({
         minWidth: '100%'
     },
     menuLateral: {
-        backgroundColor: '#cfedfc',
+        backgroundColor: corAzul1,
         height: '100%',
         width: '15%',
         minHeight: '100%',
@@ -25,7 +29,7 @@ const useStyles = makeStyles({
     },
     header: {
         position: 'relative',
-        backgroundColor: '#98d4f5',
+        backgroundColor: corAzul2,
         minHeight: '10%',
         marginLeft: '15%',
         width: '100%',
@@ -33,7 +37,7 @@ const useStyles = makeStyles({
     },
     footer: {
         position: 'relative',
-        backgroundColor: '#98d4f5',
+        backgroundColor: corAzul2,
         minHeight: '10%',
         marginLeft: '15%',
         zIndex: '1',
@@ -42,6 +46,43 @@ const useStyles = makeStyles({
     },
     conteudo: {
        
+    },
+    menuLinha: {
+        height: '1px',
+        background: corAzul3,
+        width: '90%',
+        marginLeft: '5%',
+        marginTop: '10%',
+        marginBottom: '10%'
+    },
+    menuLateralItemArrow: {
+        display: 'flex',
+        paddingLeft: '10%',
+        "&:hover": {
+            background: corAzul2,
+            cursor: 'pointer',
+        }
+    },
+    menuLateralIconArrow: {
+        transform: 'translateY(35%)',
+    },
+    menuLateralItem:{
+        marginTop: '5%',
+        marginBottom: '5%',
+        textAlign: 'left',
+    },
+    menuLateralItens:{
+        marginTop: '10%'
+    },
+    menuLateralTopo:{
+        height: '10%'
+    },
+    versao: {
+        position:'fixed',
+        bottom: '0',
+        marginBottom: '1%',
+        marginLeft: '3%',
+        color: corAzul3
     }
     
 })
